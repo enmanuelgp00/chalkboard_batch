c2ad7529fcf7c1638658bfabe90525c5bab0944a
enmanuel <enmanuelgutierrezpoey@gmail.com>
2024-09-09 16:31:53 -0400

    Agregando nitificaciones de estado al script de sincronización a github

--- upload to github.bat
+++ upload to github.bat
@@ -1,8 +1,10 @@
 @echo off
 git status
 echo.
-set /p message="type down your commit message: "
+set /p message="Type down your commit message: "
+echo Adding commit, wait ...
 git add . 
 git commit -m "%message%"
+echo Syncing with GitHub ...
 git push
 echo on
