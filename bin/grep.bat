4caf49020bd0642e74de8a8b530f37d7418bfb02
enmanuel <enmanuelgutierrezpoey@gmail.com>
2024-09-17 18:56:31 -0400

    Ahora funciona parecido a grep en linux, permite estableces cuantas lineas a leer despues de la coincidencia en la búsqueda de la salidad de un comando

--- bin/grep.bat
+++ bin/grep.bat
@@ -1,8 +1,23 @@
 @echo off
+setlocal enabledelayedexpansion
+if "%~1" equ "" (
+  echo grep [command] [search] [counter]
+) else (
+  set command=%~1
+  set search=%~2
+  set counter=%~3
+  set isFound=false
+  for /f "delims=" %%i in ('!command!') do (
+    for /f "delims=" %%a in ("%%i") do (
+      echo %%i | findstr /i "!search!" > nul && set isFound=true
+    )
+    if "!isFound!" equ "true" (
+      if !counter! gtr 0 (
+        set /a counter = counter - 1
+        echo %%i
+      )
+    )
+  )
 
-::for /f "delims=" %%i  in ('%~1') do (
-::  echo %%i | findstr /i %~2
-::)
-
-%~1 | findstr /i %~2
+)
 echo on
