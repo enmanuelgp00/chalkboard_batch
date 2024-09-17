b9c0de3b232ae9eff4f66a269fdeda34200e8815
enmanuel <enmanuelgutierrezpoey@gmail.com>
2024-09-17 18:52:17 -0400

    Bug corregido, cuando hay más de una interfaz, ahora debe recibe un parámetro que especifica la interfa, aunque todo es a base e filtrar strings

--- bin/get-gateway.bat
+++ bin/get-gateway.bat
@@ -1,25 +1,10 @@
 @echo off
 setlocal enabledelayedexpansion
-set gw=
-set isFound="no"
-for /f "delims=" %%i in ('ipconfig') do (
-  for /f "delims=: tokens=1,2" %%1 in ("%%i") do (
-
-rem Filtrando el string en busca de la palabra "defaultGateway"
-    set gw="%%1"
-    set gw=!gw: =!
-    set gw=!gw:.=!
-    if !gw!=="DefaultGateway" (
-      set gw=%%2
-      set isFound="yes"
-      goto continue
-    )
+if "%~1" equ "" (
+  echo get-gateway [interface-name]
+) else (
+  call setFromCommand var "grep ipconfig %~1 6"
+  for /f "delims=: tokens=1,2" %%a in ("!var!") do (
+    echo %%b
   )
 )
-:continue
-if !isFound!=="yes" (
-  echo !gw!
-) else (
-  echo You are not connected to a router.
-)
-setlocal disabledelayedexpansion
