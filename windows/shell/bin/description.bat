07dbcb05f700211ae0fd7b742f6a91ba323de059
enmanuel <enmanuelgutierrezpoey@gmail.com>
2024-09-25 21:37:14 -0400

    archivo creado windows/shell/bin/description.bat permite agregar strings a un numbre de archivo como posible descripción así como borralos

---
+++ windows/shell/bin/description.bat
@@ -0,0 +1,39 @@
+@echo off
+setlocal enableDelayedExpansion
+
+set tag=@%~2
+set tag=!tag: = @!
+set files=%~3
+
+if "%~1" equ "--clear" (
+
+  rem removes all the descriptions
+  if "%~2" equ "--all" (
+    for %%f in ("%files%") do (
+      set fileName=%%f
+      for /f "delims=@" %%n in ("%%f") do (
+        set newFileName=%%~nn%%~xf   
+        set newFileName=!newFileName: %%~xf=%%~xf!
+        rename "!fileName!" "!newFileName!"
+      )
+    )
+  ) else (
+    
+    rem removes description
+    for %%f in ("%files%") do (
+      set fileName=%%~nf%%~xf
+      set "newFileName=!fileName: %tag%=!"
+      rem echo !fileName! !newFileName!
+      rename "!fileName!" "!newFileName!"
+    )
+  )
+  
+  rem add description
+) else if "%~1" equ "--add" (
+  for %%f in ("%files%") do (
+    rename "%%~nf%%~xf" "%%~nf %tag%%%~xf"
+  )
+) else (
+  echo.
+  echo description [--clear/add] [strDescription] [strFile^(s^)]
+)
